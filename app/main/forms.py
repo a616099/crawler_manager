255ff48378b17ac79f324b838c7788be2852db47
a616099 <a616099@live.com>
2018-11-30 17:53:21 +0800

    project start

---
+++ app/main/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validatiors import DataRequired, Length
+from wtforms import ValidationError
+from ..models import Config, Tasks
+
+
+class ConfigForm(FlaskForm):
+    name = StringField('程序名称', validators=[DataRequired(), Length(1, 64)])
+    owner = StringField('采集负责人', validators=[DataRequired(), Length(1, 64)])
+    cycle = StringField('采集周期', validators=[DataRequired(), Length(1, 64)])
+    detail_time = StringField('具体采集时间', validators=[DataRequired(), Length(1, 64)])
+    task_table_name = StringField('任务表名', validators=[DataRequired(), Length(1, 64)])
+    task_table_time = StringField('任务表时间字段', validators=[DataRequired(), Length(1, 64)])
+    submit = SubmitField('确定')
---
+++ app/models.py
@@ -0,0 +1,19 @@
+from . import db
+
+class Config(db.Model):
+    __table__ = 'crawler_manager_config'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    owner = db.Column(db.String(64), unique=True)
+    cycle = db.Column(db.String(64), unique=True)
+    detail_time = db.Column(db.String(64), unique=True)
+    task_table_name = db.Column(db.String(64), unique=True)
+    task_table_time = db.Column(db.String(64), unique=True)
+
+
+class Tasks(db.Model):
+    __table__ = 'crawler_manager_tasks'
+    id = db.Column(db.Integer, primary_key=True)
+    config_id = db.Column(db.Integer, db.ForeignKey('crawler_manager_config.id'))
+    collect_time = db.Column(db.DateTime, unique=True)
+    data_count = db.Column(db.integer, unique=True)
