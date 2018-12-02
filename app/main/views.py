3c85f19342c0d8b9ac38ae99fd4d8cf2e34184c7
a616099 <a616099@live.com>
2018-12-02 22:11:34 +0800

    views add

--- app/main/views.py
+++ app/main/views.py
@@ -1,2 +1,19 @@
 from flask import render_template, redirect
 from .. import db
+from .froms import ConfigForm
+from ..models import Config
+
+@main.route('/config', methods=['GET', 'POST'])
+def config():
+    form = ConfigForm()
+    if form.validate_on_submit():
+        config = Config(name=form.name.data,
+                        owner=form.owner.data,
+                        cycle=form.cycle.data,
+                        detail_time=form.detail_time.data,
+                        task_table_name=form.task_table_name.data,
+                        task_table_time=form.task_table_time.data,)
+        db.session.add(config)
+        db.seesion.commit()
+        return redirect(url_for())
+    return render_template('', form=form)
